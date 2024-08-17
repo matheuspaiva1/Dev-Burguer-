b57cee621001d2303d1de4f623c9b3a027c8695f
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-08-17 14:21:55 -0300

    feat: add funcionalidade de abrir modal

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log('teste')
+
+const menu = document.getElementById('menu')
+
+const cartBtn = document.getElementById('cart-btn')
+
+const cartModal = document.getElementById('cart-modal')
+
+const cartItemsContainer = document.getElementById('cart-items')
+
+const cartTotal = document.getElementById('cart-total')
+
+const checkoutBtn = document.getElementById('checkout-btn')
+
+const closeModalBtn = document.getElementById('close-modal-btn')
+
+const cartCounter = document.getElementById('cart-count')
+
+const addresInput = document.getElementById('addres')
+
+const addressWarn = document.getElementById('address-warn')
+
+//abrir modal carrinho
+cartBtn.addEventListener('click', () =>{
+  cartModal.style.display = 'flex';
+})
+
+
+
+
+
+
+
+
+console.log(menu)
